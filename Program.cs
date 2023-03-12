c184e5fcd7c018de10bac17cfca58ddfe56b27da
ilyaBobrov <rog89@bk.ru>
2023-03-12 22:36:23 +0500

    Input data

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿#nullable disable
+int taskLength = 3;  // Задается по условию задания
+Console.Write("Enter numbers strings: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] inArray = new string [size];
+for (int i = 0; i < inArray.Length; i++)
+{
+    System.Console.Write($"Enter string №{i+1}: ");
+    inArray[i] = Console.ReadLine();
+}
+System.Console.Write("Input Array strings: ");
+System.Console.WriteLine(String.Join(", ", inArray));
